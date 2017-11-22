a64d11cfd5709765a25268d4b9742fa396e4b6cb
Tit Petric <tit.petric@monotek.net>
2017-11-22 11:17:22 +0100

    build 32 and 64 bit versions

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --net=party -e CGO_ENABLED=0 --rm=true -i -v $(pwd):/go/src/smartcrop -w /go/src/smartcrop golang:1.9-alpine go build main.go
+docker run --net=party -e GOOS=linux -e GOARCH=386 -e CGO_ENABLED=0 --rm=true -i -v $(pwd):/go/src/smartcrop -w /go/src/smartcrop golang:1.9-alpine go build -o main32 main.go
+docker run --net=party -e GOOS=linux -e GOARCH=amd64 -e CGO_ENABLED=0 --rm=true -i -v $(pwd):/go/src/smartcrop -w /go/src/smartcrop golang:1.9-alpine go build -o main64 main.go
